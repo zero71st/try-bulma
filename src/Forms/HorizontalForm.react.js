bd00f3deedd1258c3643cb5e22983ca7063a0468
kasem <kasem.w@dzcard.com>
2018-09-20 08:18:53 +0700

    Update horizontal form#1

--- src/Forms/HorizontalForm.react.js
+++ src/Forms/HorizontalForm.react.js
@@ -72,25 +72,142 @@ class HorizontalForm extends React.Component {
                             </div>
                         </div>
                     </div>
+
                     <div className="field is-horizontal">
                         <div className="field-label">
-                            <label className="label is-normal">Already a member?</label>
+                            <label className="label">Already a member?</label>
                         </div>
                         <div className="field-body">
                             <div className="field is-narrow">
                                 <div className="control">
                                     <label className="radio">
-                                        <input type="radio" name="Member"></input>
+                                        <input type="radio" name="member" />
                                         Yes
                                     </label>
                                     <label className="radio">
-                                        <input type="radio"/>
+                                        <input type="radio" name="member" />
                                         No
                                     </label>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                    <div className="field is-horizontal">
+                        <div className="field-label is-normal">
+                            <label className="label">Subject</label>
+                        </div>
+                        <div className="field-body">
+                            <div className="field">
+                                <div className="control">
+                                    <input className="input is-danger" placeholder="E.g partnership opportunity"></input>
+                                </div>
+                                <p className="help is-danger">
+                                    this field is require.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="field is-horizontal">
+                        <div className="field-label is-normal">
+                            <label className="label">Question</label>
+                        </div>
+                        <div className="field-body">
+                            <div className="field">
+                                <div className="control">
+                                    <textarea className="textarea" placeholder="Tell me how we can help you"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="field is-horizontal">
+                        <div className="field-label"></div>
+                        <div className="field-body">
+                            <div className="field">
+                                <div className="control">
+                                    <button className="button is-success">Send message</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="container">
+                    <h1 className="title">Size of horizontal form</h1>
+                    <h2 className="subtitle">How to use variansize</h2>
+
+                    <div className="field is-horizontal">
+                        <div className="field-label">
+                            <label className="label">No padding</label>
+                        </div>
+                        <div className="field-body">
+                            <div className="field">
+                                <div className="control">
+                                    <label className="checkbox">
+                                        <input type="checkbox" />
+                                        Checkbox
+                                     </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="field is-horizontal">
+                        <div className="field-label is-small">
+                            <label className="label">Small input</label>
+                        </div>
+                        <div className="field-body">
+                            <div className="field">
+                                <div className="control">
+                                    <input className="input is-small" type="text" placeholder="Small input"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="field is-horizontal">
+                        <div className="field-label">
+                            <label className="label">Normal input</label>
+                        </div>
+                        <div className="field-body">
+                            <div className="field">
+                                <div className="control">
+                                    <input className="input" placeholder="Normal input"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <dic className="field is-horizontal">
+                        <div className="field-label is-medium">
+                            <label className="label">Medium input</label>
+                        </div>
+                        <div className="field-body">
+                            <div className="field">
+                                <div className="control">
+                                    <input className="input is-medium" placeholder="Medium input" />
+                                </div>
+                            </div>
+                        </div>
+                    </dic>
+
+
+                    <div className="field is-horizontal">
+                        <div className="field-label is-large">
+                            <label className="label">Large input</label>
+                        </div>
+                        <div className="field-body">
+                            <div className="field">
+                                <div className="control">
+                                    <input className="input is-large" placeholder="Large input" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </section>
         );
